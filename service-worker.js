9cc6a37174ec63babd32883131d534957037441d
dewikahn <dewi2005@icloud.com>
2026-07-27 10:09:56 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'my-places-v1';
+const APP_SHELL = [
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first for the app shell, cache-first fallback for map tiles / CDN assets
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, resClone)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req))
+  );
+});
